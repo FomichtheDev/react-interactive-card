b2c919f3c938184bfdbb4caa07207778e08c2de7
FomichtheDev <fomichg@gmail.com>
2020-08-31 22:54:02 +0300

    Updates

---
+++ precache-manifest.340983a9534099c8cf13b384603eb5d7.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6d5b989600645b93176fa8c681af96",
+    "url": "/react-interactive-card/index.html"
+  },
+  {
+    "revision": "5afe5676eeb396afc719",
+    "url": "/react-interactive-card/static/css/main.78890e13.chunk.css"
+  },
+  {
+    "revision": "e0ce6a550c4e7f73e80b",
+    "url": "/react-interactive-card/static/js/2.66c578c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-interactive-card/static/js/2.66c578c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5afe5676eeb396afc719",
+    "url": "/react-interactive-card/static/js/main.bd7b1f6d.chunk.js"
+  },
+  {
+    "revision": "6c1f6e3d3a6fbb97caf4",
+    "url": "/react-interactive-card/static/js/runtime-main.4ed477b4.js"
+  },
+  {
+    "revision": "dd976805e1c1573ba2e6b6faf9f56541",
+    "url": "/react-interactive-card/static/media/1.dd976805.jpeg"
+  },
+  {
+    "revision": "a0c83af51972c056ede59a1e488fe57e",
+    "url": "/react-interactive-card/static/media/10.a0c83af5.jpeg"
+  },
+  {
+    "revision": "7347d17081beba503b57c80d92d988ab",
+    "url": "/react-interactive-card/static/media/11.7347d170.jpeg"
+  },
+  {
+    "revision": "83d3b0df446ff19a5ca726715b0498b1",
+    "url": "/react-interactive-card/static/media/12.83d3b0df.jpeg"
+  },
+  {
+    "revision": "d8d7c0e50ce711f0834220b771cc03bd",
+    "url": "/react-interactive-card/static/media/13.d8d7c0e5.jpeg"
+  },
+  {
+    "revision": "17c283b139cd1066de6641c1cd42ded2",
+    "url": "/react-interactive-card/static/media/14.17c283b1.jpeg"
+  },
+  {
+    "revision": "9e17b96b7a70fc8ac9160be2bcd67f94",
+    "url": "/react-interactive-card/static/media/15.9e17b96b.jpeg"
+  },
+  {
+    "revision": "eef3dd17ec5bb4f0fe9989450bc37825",
+    "url": "/react-interactive-card/static/media/16.eef3dd17.jpeg"
+  },
+  {
+    "revision": "625d9dfd8b297e5637d6f14e0fc84fa0",
+    "url": "/react-interactive-card/static/media/17.625d9dfd.jpeg"
+  },
+  {
+    "revision": "9801848a65becfb59a1ebc7f204f1e1b",
+    "url": "/react-interactive-card/static/media/18.9801848a.jpeg"
+  },
+  {
+    "revision": "db8a2b71d19d9e2ece2b47896bf01575",
+    "url": "/react-interactive-card/static/media/19.db8a2b71.jpeg"
+  },
+  {
+    "revision": "0a55c9d5e440cf2256a0efa58f98621c",
+    "url": "/react-interactive-card/static/media/2.0a55c9d5.jpeg"
+  },
+  {
+    "revision": "d7d8f6e6c9bf8c929ed071ac06a32b28",
+    "url": "/react-interactive-card/static/media/20.d7d8f6e6.jpeg"
+  },
+  {
+    "revision": "cd0636da63f755981c343ca26a769f17",
+    "url": "/react-interactive-card/static/media/21.cd0636da.jpeg"
+  },
+  {
+    "revision": "1a26d6c3f53d1bd77a48162b58c9e280",
+    "url": "/react-interactive-card/static/media/22.1a26d6c3.jpeg"
+  },
+  {
+    "revision": "4916215a09960d60f9dbe9a444df4eca",
+    "url": "/react-interactive-card/static/media/23.4916215a.jpeg"
+  },
+  {
+    "revision": "2555dbb51f045590442c6f841fb2c58c",
+    "url": "/react-interactive-card/static/media/24.2555dbb5.jpeg"
+  },
+  {
+    "revision": "5b2a3e2910e186151a6557042383fbe5",
+    "url": "/react-interactive-card/static/media/25.5b2a3e29.jpeg"
+  },
+  {
+    "revision": "96fa375f4277e32d7ef62005d76fe2c7",
+    "url": "/react-interactive-card/static/media/3.96fa375f.jpeg"
+  },
+  {
+    "revision": "ed09586227a4f01b9548a4a478b95ce8",
+    "url": "/react-interactive-card/static/media/4.ed095862.jpeg"
+  },
+  {
+    "revision": "c5d4f6f5bb10b6843507854a5b281531",
+    "url": "/react-interactive-card/static/media/5.c5d4f6f5.jpeg"
+  },
+  {
+    "revision": "c55f380c57b10c038b0c7f7182089371",
+    "url": "/react-interactive-card/static/media/6.c55f380c.jpeg"
+  },
+  {
+    "revision": "87daa240b240d76d2adbe5d3f5ad7fa1",
+    "url": "/react-interactive-card/static/media/7.87daa240.jpeg"
+  },
+  {
+    "revision": "1389dd96a7d8da683e087d747905da86",
+    "url": "/react-interactive-card/static/media/8.1389dd96.jpeg"
+  },
+  {
+    "revision": "2cc830577fe4c062aea5d48c00cead49",
+    "url": "/react-interactive-card/static/media/9.2cc83057.jpeg"
+  },
+  {
+    "revision": "2c37b1d33b45b2158112bb8172af579b",
+    "url": "/react-interactive-card/static/media/chip.2c37b1d3.png"
+  },
+  {
+    "revision": "62706277b8600fc43afcdf69be858c07",
+    "url": "/react-interactive-card/static/media/discover.62706277.png"
+  },
+  {
+    "revision": "87087e5e777b4dc7859e35dcfd8d77aa",
+    "url": "/react-interactive-card/static/media/jcb.87087e5e.png"
+  },
+  {
+    "revision": "cdaba15c87239cfe3126dda858c2078f",
+    "url": "/react-interactive-card/static/media/mastercard.cdaba15c.png"
+  },
+  {
+    "revision": "40ead9adb1960b406a610a268acf0ebc",
+    "url": "/react-interactive-card/static/media/unionpay.40ead9ad.png"
+  }
+]);
